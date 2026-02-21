d25a72805a22d8e3d5b3fcdf59e388c597b229ea
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-21 17:21:11 -0600

    setup the seeddata class that is used in our program  for our activity table in the database.

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4c1a6d616781c5503a9958e06fbe1ffb5a6dbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655be38361b20e132597115d8f1dffd97544d74a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

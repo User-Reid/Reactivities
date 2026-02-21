fb5cdc6397350f7f1892cb3bb84444a4ee5837c7
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-21 11:17:09 -0600

    created the Activity class object and added its properties.

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb42f75a365f705dd34727cb0a39e385920a6c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e72f10719806ee1186f3d15bd762e83423d42a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

1a0d57286a60d5f57d975ba25a81065d68b60002
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-22 14:10:58 -0600

    added a gitignore file to exclude certain items when publishing

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1d42ab7267286dcddfdde06153dab6268ff2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682f253ac3171fd68443ffc2a47297328a3bdc31")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

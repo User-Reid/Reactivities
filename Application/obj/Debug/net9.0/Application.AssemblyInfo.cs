aaa2a1e755e97643b2796f0127c7fc92f421be79
Reid Herrera <reidherrerabusiness@gmail.com>
2026-02-22 19:03:38 -0600

    added some styling to our application through installing some packages from mui.com. After installing them, I imported them and got rid of the traditional elements in exchanged for the prestyled ones from mui.

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0d57286a60d5f57d975ba25a81065d68b60002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d3cb27018da0ef2e4ed7bd332a21253e0815dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+import { ListItem, Typography, List, ListItemText } from "@mui/material";
+
 function App() {
-  return <h3>Reactivites</h3>;
+  const title: string = "Welcome to Reactivities";
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/activities").then((response) =>
+      response.json().then((data) => setActivities(data)),
+    );
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h3" style={{ color: "blue" }}>
+        {title}
+      </Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
 }
 
 export default App;
